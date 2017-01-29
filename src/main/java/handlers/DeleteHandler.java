a0044ec33a603c7afbe2b26b287d768ed828681a
CJ Miller <cjmiller721@gmail.com>
2017-01-29 18:23:40 -0500

    Initial Commit

---
+++ src/main/java/handlers/DeleteHandler.java
@@ -0,0 +1,37 @@
+package handlers;
+
+import dynamic.handler.IDeleteHandler;
+import protocol.HttpRequest;
+import protocol.Protocol;
+import protocol.response.DeleteResponse;
+import protocol.response.IHttpResponse;
+import utils.AccessLogger;
+
+import java.io.File;
+
+public class DeleteHandler implements IDeleteHandler {
+
+	private String rootDirectory;
+	
+	public DeleteHandler(String rootDirectory) {
+		this.rootDirectory = rootDirectory;
+	}
+
+	public IHttpResponse handleDelete(HttpRequest request) {
+		
+		String uri = request.getUri();
+		
+		File file = new File(rootDirectory + uri);
+		
+		//File to delete exists
+		if(file.exists()) {
+			file.delete();
+			return DeleteResponse.get200(file, Protocol.CLOSE);
+		} else {
+			AccessLogger.getInstance().info("File does not exists! Delete Handler Invoked!");
+			return DeleteResponse.get404(Protocol.CLOSE);
+		}
+		
+	}
+
+}
---
+++ src/main/java/handlers/GetHandler.java
@@ -0,0 +1,56 @@
+package handlers;
+
+import dynamic.handler.IGetHandler;
+import protocol.HttpRequest;
+import protocol.Protocol;
+import protocol.response.GetResponse;
+import protocol.response.IHttpResponse;
+
+import java.io.File;
+
+public class GetHandler implements IGetHandler {
+
+	private String rootDirectory;
+
+	public GetHandler(String rootDirectory) {
+		this.rootDirectory = rootDirectory;
+	}
+
+	public IHttpResponse handleGet(HttpRequest request) {
+		IHttpResponse response;
+		// Map<String, String> header = request.getHeader();
+		// String date = header.get("if-modified-since");
+		// String hostName = header.get("host");
+		//
+		// Handling GET request here
+		// Get relative URI path from request
+		String uri = request.getUri();
+		// Get root directory path from server
+		// Combine them together to form absolute file path
+		File file = new File(rootDirectory, uri);
+		// Check if the file exists
+		if (file.exists()) {
+			if (file.isDirectory()) {
+				// Look for default index.html file in a directory
+				String location = rootDirectory + uri + System.getProperty("file.separator") + Protocol.DEFAULT_FILE;
+				file = new File(location);
+				if (file.exists()) {
+					// Lets create 200 OK response
+					response = GetResponse.get200(file, Protocol.CLOSE);
+				} else {
+					// File does not exist so lets create 404 file not found
+					// code
+					response = GetResponse.get404(Protocol.CLOSE);
+				}
+			} else { // Its a file
+						// Lets create 200 OK response
+				response = GetResponse.get200(file, Protocol.CLOSE);
+			}
+		} else {
+			// File does not exist so lets create 404 file not found code
+			response = GetResponse.get404(Protocol.CLOSE);
+		}
+		return response;
+	}
+
+}
